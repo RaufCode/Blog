d4d7b2b9ae544d5c532791af53559c5ce50c6ddd
RaufCode <abdulraufsaeed923@gmail.com>
2026-07-24 21:25:06 +0000

    feat: add PostUpdate schema

--- app/schemas/post.py
+++ app/schemas/post.py
@@ -5,3 +5,9 @@ class PostCreate(BaseModel):
     title: str
     content: str
     author: str | None = None
+
+
+class PostUpdate(BaseModel):
+    title: str | None = None
+    content: str | None = None
+    author: str | None = None
