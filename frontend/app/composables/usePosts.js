26599075de423b9b88bf7c5dd1b55604a781ae73
RaufCode <abdulraufsaeed923@gmail.com>
2026-07-27 09:44:44 +0000

    feat: add post mutation composables

--- frontend/app/composables/usePosts.js
+++ frontend/app/composables/usePosts.js
@@ -7,3 +7,25 @@ export function usePosts() {
 export function usePost(id) {
   return useFetch(`/api/post/${id}`)
 }
+
+
+export function useCreate(blog_info) {
+  return useFetch(`/api/post/create`, {
+    method: 'POST',
+    body: blog_info,
+  })
+}
+
+export function useDelete(id) {
+  return useFetch(`/api/post/${id}`, {
+    method: 'DELETE',
+    body: id,
+  })
+}
+
+export function useUpdate(id, blog_info) {
+  return useFetch(`/api/post/${id}`, {
+    method: 'PUT',
+    body: blog_info,
+  })
+}
